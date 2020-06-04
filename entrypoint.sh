91c774b13eb84e4d85c8215d3e682428a8eba510
darksaid98 <darksaid9889@gmail.com>
2020-06-04 06:21:31 +0200

    Update entrypoint.sh to auto clear cache for FiveM

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,14 @@ export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
+# Auto Clearing Cache for FiveM Servers
+if [ -d "/home/container/cache/" ] && [ -d "/home/container/cache/files/" ]; then
+    echo "Clearing Cache..."
+    rm -r /home/container/cache/files/
+    sleep 5s
+    echo "Cache cleared..."
+fi
+
+
 # Run the Server
 eval ${MODIFIED_STARTUP}
