84ccda8841dbe582d3671bcdc2729d5c8d4a450a
darksaid98 <darksaid9889@gmail.com>
2020-06-10 23:48:36 +0200

    1

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ cd /home/container
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
-git clone https://${GTOKEN}@github.com/darksaid98/everlife.git /home/container/test && echo "Updated from git" || echo "Update failed"
+git clone https://$GTOKEN@github.com/darksaid98/everlife.git /home/container/test && echo "Updated from git" || echo "Update failed"
 
 # Auto clearing cache for FiveM servers
 if [ -d "/home/container/cache/" ] && [ -d "/home/container/cache/files/" ]; then
