a161244fdf790603123d5b019c6c8d4da7e923e8
darksaid98 <darksaid9889@gmail.com>
2020-06-11 00:32:29 +0200

    fa

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,10 @@ export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 #git config user.email "darksaid9889@gmail.com"
 
 sleep 1s
+cd /home/container/resources
 git pull https://${GTOKEN}@github.com/darksaid98/everlife.git /home/container/resources && echo "Updated from git" || echo "Update failed"
 
+cd /home/container
 # Auto clearing cache for FiveM servers
 if [ -d "/home/container/cache/" ] && [ -d "/home/container/cache/files/" ]; then
     echo "Wait, inspecting cache.";
