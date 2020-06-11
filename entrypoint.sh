5e9831a22f2fe14abac9f691e03f06e993387055
darksaid98 <darksaid9889@gmail.com>
2020-06-11 10:53:07 +0200

    Cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,17 +4,20 @@ cd /home/container
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
-sleep 1s
+# Add all the custom ENV's to your panels egg.
 
+sleep 1s
+# Auto update resources from git.
 if [ "$GIT_ENABLED" = "true" ]; then
   echo "Wait, preparing to update.";
   sleep 1s
   cd /home/container/resources
   git pull https://${GIT_USERNAME}:${GIT_TOKEN}@github.com/${GIT_USERNAME}/${GIT_REPOSITORYNAME}.git ${GIT_BRANCH} && echo "Updated server from git." || echo "Update failed."
-  cd /home/container
+  cd /home/container 
 fi
 
-# Auto clearing cache for FiveM servers
+sleep 1s
+# Auto clearing cache for FiveM servers.
 if [ "$CACHE_CLEAR" = "true" ]; then
   if [ -d "/home/container/cache/" ] && [ -d "/home/container/cache/files/" ]; then
       echo "Wait, inspecting cache.";
@@ -28,9 +31,9 @@ if [ "$CACHE_CLEAR" = "true" ]; then
   fi
 fi
 
-# Replace Startup Variables
+# Replace Startup Variables.
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-# Run the Server
+# Run the Server.
 eval ${MODIFIED_STARTUP}
